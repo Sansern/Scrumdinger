47f44fd2a4105b6c3b2d7f4fed53c1f267f66bd1
MintSW <mintnimal@gmail.com>
2022-02-20 16:33:45 +0700

    Chapter 2 : Creating a cart view. – Section 4 Customize the label style - add accessibility Voice over

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -9,16 +9,19 @@ import SwiftUI
 
 struct CardView: View {
     let scrum: DailyScrum
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text(scrum.title)
                 .font(.headline)
+                .accessibilityAddTraits(.isHeader)
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
-                    .padding(.trailing, 20.0)
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
                     .labelStyle(.trailingIcon)
                 
             }
@@ -29,6 +32,7 @@ struct CardView: View {
         
     }
 }
+
 struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.sampleData[0]
     static var previews: some View {
