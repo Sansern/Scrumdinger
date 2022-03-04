cdbe151191a1e558ff1772b67012182e53a13561
MintSW <mintnimal@gmail.com>
2022-03-04 11:37:16 +0700

    Creating a Navigation Hierarchy - Section 5 Navigate Between Screens - Add Navigation Link to Start Meeting - Add Navigation Title in Detail View

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -5,9 +5,12 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                Label("Start Meeting",systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()){
+                    Label("Start Meeting",systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
@@ -31,6 +34,7 @@ struct DetailView: View {
                 }
             }
         }
+        .navigationTitle(scrum.title)
     }
 }
 
