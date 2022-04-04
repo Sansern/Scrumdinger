b4796928855bb4e55f8f1abfcc7bf61593b8d57b
MintSW <mintnimal@gmail.com>
2022-04-04 23:25:12 +0700

    Section 4 Add Accessibility Modifiers -- 3: Add an accessibility label to the image.

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -36,6 +36,7 @@ struct DetailEditView: View {
                         }
                     }) {
                         Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
                     }
                     .disabled(newAttendeeName.isEmpty)
                 }
