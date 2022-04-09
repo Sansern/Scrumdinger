a0cb12117e7f155b9c8e00a942c6188a54bac17d
MintSW <mintnimal@gmail.com>
2022-04-09 14:42:35 +0700

    Extract the Meeting Header

--- Scrumdinger/MeetingHeaderView.swift
+++ Scrumdinger/MeetingHeaderView.swift
@@ -8,34 +8,51 @@
 import SwiftUI
 
 struct MeetingHeaderView: View {
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1}
+        return Double(secondsElapsed) / Double(totalSeconds)
+    }
+    
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    
     var body: some View {
         VStack {
-            ProgressView(value: 5, total: 15)
+            ProgressView(value: progress)
             HStack {
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
                         .font(.caption)
                     
-                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
                 }
                 Spacer()
                 VStack(alignment: .trailing) {
                     Text("Seconds Remmaining")
                         .font(.caption)
-                    Label("600", systemImage: "hourglass.tophalf.fill")
+                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
                 }
                 
             }
             .accessibilityElement(children: .ignore)
             .accessibilityLabel("Time remaning")
             .accessibilityValue("10 minutes")
+            .accessibilityValue("\(minutesRemaining) minutes")
         }
     }
 }
 
 struct MeetingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingHeaderView()
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180)
             .previewLayout(.sizeThatFits)
     }
 }
