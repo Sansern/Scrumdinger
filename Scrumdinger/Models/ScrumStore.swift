ba1dac22cb9443be41cd6359aed5a98cb874dc33
MintSW <mintnimal@gmail.com>
2022-05-07 13:54:18 +0700

    Modernizing Asynchronous Code

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -16,6 +16,19 @@ class ScrumStore: ObservableObject {
             .appendingPathComponent("srums.data")
     }
     
+    static func load() async throws -> [DailyScrum] {
+        try await withCheckedContinuation { continuation in
+            load { result in
+                switch result {
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            case .success(let scrums):
+                continuation.resume(returning: scrums)
+            }
+        }
+    }
+    
     static func load(completion: @escaping (Result<[DailyScrum], Error>) -> Void) {
         DispatchQueue.global(qos: .background).async {
             do {
@@ -35,6 +48,20 @@ class ScrumStore: ObservableObject {
                 DispatchQueue.main.async {
                                     completion(.failure(error))
                                 }
+            }
+        }
+    }
+    
+    @discardableResult
+    static func save(scrums: [DailyScrum]) async throws -> Int {
+        try await withCheckedContinuation {Continuation in
+            save(scrums: scrums) { result in
+                switch result {
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+                case .success(let scrumsSaved):
+                    continuation.resume(returning: scrumsSaved)
             }
         }
     }
